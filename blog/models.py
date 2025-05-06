00e1bbd97497862b97771c869b3e482783ecf19c
Кенжебекулы Мустафа <KenzhebekulyM@lcoast.itstep>
2025-05-06 21:47:48 +0500

    relationships

---
+++ blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Prof(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+
+    def __str__(self) -> str:
+        return self.user.username
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(Prof, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now=True)
+    def __str__(self) -> str:
+        return self.title
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    posts = models.ManyToManyField(Post, related_name='tags')
+    def __str__(self) -> str:
+        return f"{self.name}"
--- polls/models.py
+++ polls/models.py
@@ -8,9 +8,12 @@ class Question(models.Model):
                                      unique_for_date='date_time', null=False, blank=False)
     
     date_time = models.DateTimeField("data published", auto_now_add=True)
+    def __str__(self) -> str:
+        return self.question_text
 
 class Choice(models.Model):
     question_text = models.ForeignKey(Question, on_delete=models.CASCADE)
     text_choice = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    
+    def __str__(self) -> str:
+        return f"{self.text_choice}, {self.question_text}"
